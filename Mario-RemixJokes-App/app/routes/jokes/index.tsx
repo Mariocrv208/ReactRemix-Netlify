932253d6fdce20297b774edf3fc62a7242e6f0ff
Mario Rodriguez <mario.crv208@gmail.com>
2022-11-16 01:41:09 -0600

    Pre mutaciones

--- Mario-RemixJokes-App/app/routes/jokes/index.tsx
+++ Mario-RemixJokes-App/app/routes/jokes/index.tsx
@@ -1,10 +1,28 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { useLoaderData, Link } from "@remix-run/react";
+import type { Joke } from "@prisma/client";
+import { db } from "~/utils/db.server";
+
+type LoaderData = { randomJoke: Joke}
+
+export let loader: LoaderFunction = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  }); 
+  let data: LoaderData = { randomJoke}
+  return data
+}
+
 export default function JokesIndexRoute() {
+    let data = useLoaderData<LoaderData>()
     return (
       <div>
         <p>Here's a random joke:</p>
         <p>
-          I was wondering why the frisbee was getting bigger,
-          then it hit me.
+          {data.randomJoke.content}
         </p>
       </div>
     );
